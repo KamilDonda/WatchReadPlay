d5e3f9f535d88b2f23cdb3527a6290ac4eced6b1
Kamil Donda <dondakamil@gmail.com>
2021-04-18 18:21:54 +0200

    Remove RecursiveRadioButton class

--- WatchReadPlay/app/src/main/java/com/example/watchreadplay/ui/main/MainFragment.kt
+++ WatchReadPlay/app/src/main/java/com/example/watchreadplay/ui/main/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.example.watchreadplay.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.watchreadplay.R
+import kotlinx.android.synthetic.main.main_fragment.*
+
 
 class MainFragment : Fragment() {
 
@@ -24,7 +28,13 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        radio_group_bottom.setOnCheckedChangeListener { group, checkedId -> // checkedId is the RadioButton selected
+            val rb = view.findViewById(checkedId) as RadioButton
+        }
+    }
 }
