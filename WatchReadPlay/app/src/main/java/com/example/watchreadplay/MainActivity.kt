95cecb9fce31b464a0b3b3c425f686f89a1873de
Kamil Donda <dondakamil@gmail.com>
2021-04-27 14:50:16 +0200

    Set orientation as portrait, change text buttons color

--- WatchReadPlay/app/src/main/java/com/example/watchreadplay/MainActivity.kt
+++ WatchReadPlay/app/src/main/java/com/example/watchreadplay/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.watchreadplay.ui.main.MainFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -13,8 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-
-        Firebase.database.setPersistenceEnabled(true)
+        if (savedInstanceState == null) {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+        }
     }
 
     override fun onBackPressed() {}
