3019761c04e4e0cd2f77a1e11a784d1d4b72e79b
Kamil Donda <dondakamil@gmail.com>
2021-05-12 22:19:33 +0200

    Enabled signing up with email

--- WatchReadPlay/app/src/main/java/com/example/watchreadplay/fragments/RegisterFragment.kt
+++ WatchReadPlay/app/src/main/java/com/example/watchreadplay/fragments/RegisterFragment.kt
@@ -1,18 +1,63 @@
 package com.example.watchreadplay.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.watchreadplay.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
+
+    private val TAG = "RegisterFragment"
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sign_up_button.setOnClickListener {
+            val email = email.text.toString()
+            val password = password.text.toString()
+            val repeated_password = repeatPassword.text.toString()
+
+            if (password == repeated_password) {
+                createAccount(email, password)
+            }
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{ task ->
+            if (task.isSuccessful) {
+                val user = auth.currentUser
+                Log.d(TAG, user.toString())
+            } else {
+                Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                Toast.makeText(requireContext(), "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
